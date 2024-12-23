3bdc271305ade33a10b15e8a0f831791e182a81c
kurosha <144055289+kkurosha@users.noreply.github.com>
2024-12-24 01:54:49 +0300

    Add files via upload

---
+++ siaod_7/siaod_7.1/siaod_7.1.cpp
@@ -0,0 +1,216 @@
+﻿#include <iostream> 
+using namespace std; 
+
+struct Node { //структурa для узла дерева
+    int key; //инфорационное поле - целое число 
+    Node* left; //левое поддерево
+    Node* right; //правое поддерево
+    Node(int val) : key(val), left(nullptr), right(nullptr) {} //конструктор 
+};
+
+class BinarySearchTree { //класс для бинарного дерева поиска
+private:
+    Node* root; //корень дерева
+
+public:
+    BinarySearchTree() : root(nullptr) {} //конструктор для создания пустого дерева
+
+    void insert(int key) { //добавление узла
+        Node* newNode = new Node(key); //cоздаем новый узел
+        if (root == nullptr) { //если дерево пустое, то новый узел - корень
+            root = newNode;
+            return;
+        }
+        Node* current = root; //для перемещения по дереву
+        Node* parent = nullptr; //родитель
+        while (current != nullptr) { //ищем место для нового узла
+            parent = current; //запоминаем родителя
+            if (key < current->key) { current = current->left; } //переход в левое поддерево
+            else { current = current->right; } //в правое поддерево 
+        }
+        if (key < parent->key) { parent->left = newNode; } //добавляем в левое поддерево
+        else { parent->right = newNode; } //добавляем в правое поддерево
+    }
+
+    int search(int key) { //поиск по ключу
+        Node* current = root; //для перемещения по дереву
+        while (current != nullptr) { //пока не достигли конца дерева
+            if (current->key == key) { return current->key; } //ключ найден
+            else if (key < current->key) { current = current->left; } //поиск слева
+            else { current = current->right; } //поиск справа
+        }
+        return -1; //не найден ключ 
+    }
+
+    void remove(int key) { //удаление
+        Node* current = root; //для перемещения по дереву
+        Node* parent = nullptr; //родитель
+        while (current != nullptr && current->key != key) { //ищем узел для удаления
+            parent = current; //запоминаем родителя
+            if (key < current->key) current = current->left; //двигаемся влево
+            else current = current->right; //двигаемся вправо 
+        }
+        if (current == nullptr) return; //узел не найден
+        //узел не имеет поддеревьев
+        if (current->left == nullptr && current->right == nullptr) {
+            if (current == root) root = nullptr; //узел - корень
+            else if (parent->left == current) parent->left = nullptr; //узел - левый ребенок
+            else parent->right = nullptr; //узел - правый ребенок
+        }
+        //узел имеет одно поддерево
+        else if (current->left == nullptr || current->right == nullptr) {
+            Node* child = (current->left != nullptr) ? current->left : current->right; //ребенок
+            if (current == root) root = child; //если удаляем корень
+            else if (parent->left == current) parent->left = child; //узел - левый ребенок
+            else parent->right = child; //узел - правый ребенок
+        }
+        //узел имеет два поддерева
+        else { //ищем наименьший узел в правом поддереве
+            Node* successor = current->right; //начинаем с правого поддерева
+            Node* successorParent = current; //родитель для узла-наследника
+            while (successor->left != nullptr) { //ищем наименьший узел
+                successorParent = successor;
+                successor = successor->left;
+            }
+            current->key = successor->key; //копируем значение узла-наследника
+            if (successorParent->left == successor) { //удаляем узел-наследник
+                successorParent->left = successor->right; //подключаем правое поддерево
+            }
+            else successorParent->right = successor->right; //подключаем правое поддерево
+            current = successor; //текущий узел указывает на узел-наследник
+        }
+        delete current; //удаляем узел
+    }
+    /*void display() { //отображение дерева
+        Node* current = root;
+        if (current == nullptr) {
+            cout << "Дерево пустое\n";
+            return;
+        } //стек для итеративного обхода (прямой обход)
+        Node* stack[100]; //стек для хранения узлов
+        int top = -1; //индекс верхнего элемента стека
+        stack[++top] = current; //добавляем корень в стек
+        while (top >= 0) { // стек не пуст
+            current = stack[top--]; //извлекаем верхний элемент
+            cout << current->key << " "; //вывод значения узла
+            if (current->right != nullptr) { stack[++top] = current->right; } //добавляем правое дерево
+            if (current->left != nullptr) { stack[++top] = current->left; } //добавляем левое дерево
+        }
+        cout << endl; 
+    }*/
+    int height() { //высота дерева 
+        if (root == nullptr) return -1; //дерево пустое
+        int h = 0; //высота
+        Node* queue[100]; //очередь для выполнения обхода
+        int front = -1, rear = -1; //индексы вершины и конца очереди
+        queue[++rear] = root; //добавляем корень в очередь
+        while (front != rear) {
+            int count = rear - front; 
+            h++; 
+            while (count--) { 
+                Node* node = queue[++front]; //извлекаем узел из очереди
+                if (node->left != nullptr) queue[++rear] = node->left; //добавляем левого ребенка в очередь
+                if (node->right != nullptr) queue[++rear] = node->right; //добавляем правого ребенка в очередь
+            }
+        }
+        return h; 
+    }
+
+    int edgeLength() { //длина пути (кол-во ребер)
+        if (root == nullptr) return 0; //дерево пустое
+        int length = 0; //длина
+        Node* queue[100]; //очередь для обхода
+        int front = -1, rear = -1; //индексы вершины и конца очереди
+        queue[++rear] = root; //добавляем корень в очередь
+        while (front != rear) {
+            int count = rear - front; 
+            while (count--) { 
+                Node* node = queue[++front]; //извлекаем узел из очереди
+                if (node->left != nullptr) {
+                    queue[++rear] = node->left; //добавляем левого ребенка в очередь
+                    length++; //увеличиваем длину
+                }
+                if (node->right != nullptr) {
+                    queue[++rear] = node->right; //добавляем правого ребенка в очередь
+                    length++; //увеличиваем длину
+                }
+            }
+        }
+        return length; 
+    }
+
+    double average() { //среднее арифметическое
+        if (root == nullptr) return 0; //дерево пустое
+        int sum = 0; //для хранения суммы всех узлов
+        int count = 0; //для хранения количества узлов
+        Node* queue[100]; //очередь для обхода
+        int front = -1; //индекс передней части очереди
+        int rear = -1; //индекс задней части очереди
+        queue[++rear] = root; //добавляем корень дерева в очередь
+        while (front != rear) {
+            Node* node = queue[++front]; //извлекаем узел из очереди
+            sum += node->key; //добавляем значение узла к сумме
+            count++; //увеличиваем счетчик узлов
+            //добавляем детей узла в очередь
+            if (node->left != nullptr) queue[++rear] = node->left; //левое поддерево
+            if (node->right != nullptr) queue[++rear] = node->right; //правое поддерево
+        }
+        return count != 0 ? (double)sum / count : 0; 
+    }
+
+    void print(Node* t, int u) {
+        if (t == nullptr) return;
+        print(t->left, u + 1); //обход левого поддерева
+        for (int i = 0; i < u; ++i) { cout << "| "; }
+        cout << t->key << endl; 
+        print(t->right, u + 1); //обход правого поддерева
+    }
+    void display() { //вывод
+        if (root == nullptr) {
+            cout << "Дерево пустое" << endl;
+            return;
+        }
+        print(root, 0); //начало с корня
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "rus"); 
+    BinarySearchTree tree;
+    int n; //размер
+    int r;
+    cout << "Как заполнить дерево? Вручную - 1, рандомно - 2: ";
+    cin >> r;
+    cout << "\nВставка элементов в дерево\nВведите количество элементов: ";
+    cin >> n;
+    switch (r) {
+    case 1:
+        int q;
+        cout << "Введите элементы для вставки:\n";
+        for (int i = 0; i < n; i++) {
+            cin >> q;
+            tree.insert(q);
+        }
+        break;
+    case 2:
+        srand(time(0));
+        for (int i = 0; i < n; ++i) {
+            int randomValue = rand() % 1000;
+            tree.insert(randomValue);
+        }
+        break;
+    }
+    cout << "Дерево:\n";
+    tree.display();
+    cout << "Среднее арифметическое всех чисел в дереве: " << tree.average() << endl;
+    cout << "Длина пути дерева (количество ребер): " << tree.edgeLength() << endl;
+    cout << "Высота дерева: " << tree.height() << endl;
+    cout << "Поиск по ключу: " << tree.search(40);
+    cout << "\nКакой элемент удалить? ";
+    int w;
+    cin >> w;
+    tree.remove(w);
+    cout << "Дерево после удаления:\n";
+    tree.display();
+}
+//5 4 6 1 2
---
+++ siaod_7/siaod_7.1/siaod_7.22.cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <queue>
+using namespace std;
+const int INF = numeric_limits<int>::max();
+
+class Graph {
+private:
+    int vertices; //кол-во вершин в графе
+    vector<vector<int>> adjacency_matrix; //матрица смежности для представления графа
+   
+    int find(int v, vector<int>& parent) { //поиск корня вершины
+        if (parent[v] != v) {
+            parent[v] = find(parent[v], parent);
+        } //рекурсивно ищется корень и обновляется родительский узел
+        return parent[v];
+    }
+
+    void unite(int v1, int v2, vector<int>& parent) { //объединение двух вершин
+        parent[find(v1, parent)] = find(v2, parent);
+    } //находит корни для обеих вершин и устанавливает родителя v1 как родителя v2
+
+public:
+    Graph(int vertices) { //конструктор
+        this->vertices = vertices; //устанавливаем кол-вол вершин 
+        adjacency_matrix.resize(vertices, vector<int>(vertices, 0)); //инициализируем матрицу нулями
+    }
+    
+    void addEdge(int u, int v, int weight) { //добавление одного ребра
+        if (u <= vertices && v <= vertices && u > 0 && v > 0) {
+            adjacency_matrix[u - 1][v - 1] = weight; //индексация с 0
+            adjacency_matrix[v - 1][u - 1] = weight; //для неориентированного графа
+        }
+        else {
+            cout << "Некорректные вершины!\n";
+        }
+    }
+
+    void addEdgesBulk(int edges) { //для массового ввода ребер
+        cout << "Введите рёбра в формате 'начало конец вес':\n";
+        for (int i = 0; i < edges; ++i) {
+            int u, v, weight;
+            cout << "Ребро " << i + 1 << ": ";
+            cin >> u >> v >> weight;
+            addEdge(u, v, weight);
+        }
+    }
+
+    void display() { //вывод
+        for (int i = 0; i < vertices; i++) { //по строкам матрицы
+            for (int j = 0; j < vertices; j++) { //по столбцам
+                cout << adjacency_matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    
+    int find_center() {
+        for (int k = 0; k < vertices; k++) {
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    if (adjacency_matrix[i][k] < INF && adjacency_matrix[k][j] < INF) {
+                        adjacency_matrix[i][j] = min(adjacency_matrix[i][j],
+                            adjacency_matrix[i][k] + adjacency_matrix[k][j]);
+                    }
+                }
+            }
+        }
+        vector<int> max_distances(vertices);
+        for (int i = 0; i < vertices; i++) {
+            max_distances[i] = *max_element(adjacency_matrix[i].begin(), adjacency_matrix[i].end());
+        }
+        int center_index = distance(max_distances.begin(), min_element(max_distances.begin(), max_distances.end()));
+        return center_index;
+    }
+
+    pair<vector<pair<int, int>>, int> kruskal() { //алгоритм Крускала
+        vector<int> parent(vertices); //для хранения родителей вершин
+        for (int i = 0; i < vertices; i++) { //инициализируем родителей, 
+            parent[i] = i;         //каждая вершина — родитель сама себе
+        }
+        vector<pair<int, pair<int, int>>> edges; //для хранения всех ребер
+        for (int i = 0; i < vertices; i++) { //проходим по всем вершинам
+            for (int j = i; j < vertices; j++) { //по всем остальным вершинам
+                if (adjacency_matrix[i][j] != 0) { //есть ли ребро
+                    edges.push_back({ adjacency_matrix[i][j], {i, j} }); //добавляем ребро с весом в вектор
+                }
+            }
+        }
+        sort(edges.begin(), edges.end()); //сортируем ребра по весу
+        int mst_weight = 0; //общий вес остовного дерева
+        vector<pair<int, int>> mst_edges; //для хранения ребер остовного дерева
+        for (auto edge : edges) { //по всем ребрам
+            int weight = edge.first; //вес ребра
+            int start = edge.second.first; //стартовая вершина
+            int end = edge.second.second;  //конечная вершина
+            if (find(start, parent) != find(end, parent)) { //если вершины разных компонентов
+                unite(start, end, parent); //объединям их
+                mst_weight += weight;//общий вес +
+                mst_edges.push_back({ start, end }); //добавляем ребро в остовное дерево
+            }
+        }
+        return { mst_edges, mst_weight }; //ребра и вес остовного дерева
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int vertices;
+    cout << "Количество вершин в графе: ";
+    cin >> vertices;
+    Graph graph(vertices);
+    int choice;
+    do {
+        cout << "\n1. Добавить одно ребро\n";
+        cout << "2. Добавить несколько рёбер\n";
+        cout << "3. Найти центр графа\n";
+        cout << "4. Реализация алгоритма Крускала построения остовного дерева минимального веса\n";
+        cout << "5. Вывести матрицу смежности\n";
+        cout << "0. Выход\n";
+        cout << "Что хотите сделать?: ";
+        cin >> choice;
+        switch (choice) {
+        case 1: {
+            int u, v, weight;
+            cout << "Введите ребро (начало конец вес): ";
+            cin >> u >> v >> weight;
+            graph.addEdge(u, v, weight);
+            break;
+        }
+        case 2: {
+            int edges;
+            cout << "Введите количество ребер: ";
+            cin >> edges;
+            graph.addEdgesBulk(edges);
+            break;
+        }
+        case 3: {
+            int center = graph.find_center();
+            cout << "Центр графа: вершина " << center + 1 << endl;
+            break;
+        }
+        case 4: {
+            cout << "Алгоритм Крускала\n";
+            pair<vector<pair<int, int>>, int> result = graph.kruskal();
+            vector<pair<int, int>> mst_edges = result.first;
+            int mst_weight = result.second;
+            cout << "\nРебра остовного дерева (минимальный вес):\n";
+            for (const auto& edge : mst_edges) {
+                int start = edge.first;
+                int end = edge.second;
+                cout << start << " - " << end << endl;
+            }
+            cout << "Общий вес остовного дерева: " << mst_weight << endl;
+            break;
+        }
+        case 5: {
+            graph.display();
+            break;
+        }
+        case 0:
+            break;
+        default:
+            cout << "Попробуйте снова\n";
+        }
+    } while (choice != 0);
+}
+
